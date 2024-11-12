334b8f7659267803b5c3c109b905cf1ac75d672d
deep.oza <deep.oza@aurumproptech.in>
2024-11-12 17:22:40 +0530

    Added Privacy Screen UI.

---
+++ lib/views/screens/dashboard/profile/about/privacy_screen.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class PrivacyPolicyScreen extends StatefulWidget {
+  const PrivacyPolicyScreen({super.key});
+
+  @override
+  State<PrivacyPolicyScreen> createState() => _PrivacyPolicyScreenState();
+}
+
+class _PrivacyPolicyScreenState extends State<PrivacyPolicyScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer.run(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container();
+  }
+}
